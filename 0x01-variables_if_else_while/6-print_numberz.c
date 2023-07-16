ddcb63de76e1abe6ba1858afee4e028ea3b9106c
henok-yes <henokakalu1994@gmail.com>
2023-07-16 10:01:44 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main -  prints number
+*description:  prints zero to nine.
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x;
+for (x = 48; x < 58; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
