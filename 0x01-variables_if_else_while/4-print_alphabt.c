16f726233dbfbb916b3524be0698e463bffd2bce
henok-yes <henokakalu1994@gmail.com>
2023-07-16 08:18:15 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main -  prints the alphabet in lowercase exept e and q.
+*description:   prints the alphabet in lowercase.
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char smallAlphabt[24] = "abcdfghijklmnoprstuvwxyz";
+int x;
+for (x = 0; x <= 23; x++)
+{
+putchar(smallAlphabt[x]);
+}
+putchar('\n');
+return (0);
+}
