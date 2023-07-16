04f2ea85bf829798db0e3061cc64d234ddc2790d
henok-yes <henokakalu1994@gmail.com>
2023-07-16 11:10:48 -0700

    9-print_comb.c .c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main -Write a program that prints all possible
+*combinations of single-digit numbers
+*description: list of number
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+if (i != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('$');
+putchar('\n');
+return (0);
+}
